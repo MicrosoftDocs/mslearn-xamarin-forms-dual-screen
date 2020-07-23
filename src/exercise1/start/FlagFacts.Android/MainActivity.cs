a7a2df0321750f96d4967f96333e74d90c4bb918
Craig Dunn <craig.dunn@microsoft.com>
2020-07-23 12:51:59 -0700

    update nuget and android api level

--- src/exercise1/start/FlagFacts.Android/MainActivity.cs
+++ src/exercise1/start/FlagFacts.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace FlagFacts.Droid
 {
-    [Activity(Label = "FlagFacts", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FlagFactsDuo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
